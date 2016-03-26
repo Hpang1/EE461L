84ddf8c3a6921eddaafd89680feb8d81dc362de8
mgriffin1994 <mgriff94@utexas.edu>
2016-03-26 16:24:43 -0500

    Added activity and intent methods

--- UTest/app/src/main/java/com/team8/utest/MainActivity.java
+++ UTest/app/src/main/java/com/team8/utest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.team8.utest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,8 +17,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+    }
+
+    public void goSearch(View view){
+        Intent intent = new Intent(this, QuizSearch.class);
+        startActivity(intent);
+    }
+
+    public void goCreate(View view){
+        Intent intent = new Intent(this, QuizCreator.class);
+        startActivity(intent);
+    }
+
+    public void goResults(View view){
+        Intent intent = new Intent(this, QuizResults.class);
+        startActivity(intent);
+    }
+
+    public void goTake(View view){
+        Intent intent = new Intent(this, QuizTaker.class);
+        startActivity(intent);
     }
 
     @Override
